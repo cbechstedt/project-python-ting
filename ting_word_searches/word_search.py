65b6e5a2a39fc12949d24db4d54b108d978ea3a4
Christian <chrisnebech@hotmail.com>
2023-06-27 17:39:07 -0300

    requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -27,5 +27,29 @@ def exists_word(word, instance: Queue):
     return word_search_list
 
 
-def search_by_word(word, instance):
-    ...
+def search_by_word(word, instance: Queue):
+    word_search_list = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        file_name = file["nome_do_arquivo"]
+        file_lines = file["linhas_do_arquivo"]
+
+        occurrence_list = []
+
+        for line_index, line_content in enumerate(file_lines, start=1):
+            if word.lower() in line_content.lower():
+                occurrence_list.append(
+                    {"linha": line_index, "conteudo": line_content}
+                )
+
+        if occurrence_list:
+            word_search_list.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": occurrence_list,
+                }
+            )
+
+    return word_search_list
