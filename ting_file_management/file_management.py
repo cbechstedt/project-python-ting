408795b12b268f3a30dc4ce5d25920452f7d12ed
Christian <chrisnebech@hotmail.com>
2023-08-01 18:57:39 -0300

    .

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,7 +1,19 @@
 import sys
 
 
+def test():
+    array = [1, 2, 3, 4, 5]
+    is_3_in_array = 3 in array
+    print(is_3_in_array)
+
+
+test()
+
+
 def txt_importer(path_file):
+    array = [1, 2, 3, 4, 5]
+    is_3_in_array = 3 in array
+    print(is_3_in_array)
     if not path_file.endswith(".txt"):
         print("Formato inválido", file=sys.stderr)
     try:
